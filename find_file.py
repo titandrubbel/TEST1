48871a5d71e1f8ae2cca47afaf7544997041ecea
Rutger Drubbel <55639101+titandrubbel@users.noreply.github.com>
2020-03-13 16:32:23 +0100

    Update and rename test.py to find_file.py

--- find_file.py
+++ find_file.py
@@ -2,11 +2,12 @@ import requests
 import os
 
 url = "http://localhost:5000/api/classification/classify"
-fin = open('example.yaml', 'rb')
-files = {'file': fin}
 
-#files = os.listdir("/projects/test")
-#files = {'file': open('{}'.format(files[1]), 'rb')}
+#fin = open('example.yaml', 'rb')
+#files = {'file': fin}
+
+files = os.listdir("/projects/test")
+files = {'file': open('{}'.format(files[1]), 'rb')}
 
 r = requests.post(url, files=files)
 print(r.status_code)
