8fcfdd62d6c9c62af11f5e06f7c30324a7bd12a9
Rutger Drubbel <55639101+titandrubbel@users.noreply.github.com>
2020-03-13 14:15:24 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,16 @@
 import requests
+import os
 
 url = "http://localhost:5000/"
 fin = open('main.py', 'rb')
 files = {'file': fin}
 
+#files = os.listdir("/projects/test")
+#files = {'file': open('{}'.format(files[1]), 'rb')}
+
 try:
   r = requests.post(url, files=files)
-	print r.text
+	(r.status_code)
+	print(r.text)
 finally:
 	fin.close()
