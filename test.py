335135d3d77923bb906b89dd1e5aa2011b2ca130
Rutger Drubbel <55639101+titandrubbel@users.noreply.github.com>
2020-03-13 15:20:45 +0100

    Update test.py

--- test.py
+++ test.py
@@ -2,15 +2,12 @@ import requests
 import os
 
 url = "http://localhost:5000/"
-fin = open('main.py', 'rb')
+fin = open('example.yaml', 'rb')
 files = {'file': fin}
 
 #files = os.listdir("/projects/test")
 #files = {'file': open('{}'.format(files[1]), 'rb')}
 
-try:
-  r = requests.post(url, files=files)
-	(r.status_code)
-	print(r.text)
-finally:
-	fin.close()
+r = requests.post(url, files=files)
+print(r.status_code)
+print(r.text)
